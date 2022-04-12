5006d0de0e2b71bff1a01af494833c924307765d
Ivan Gorbunov <i.gorbunov@corp.101xp.com>
2022-04-12 20:37:10 +0300

    56 Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Tag, P, Htag, Button, Rating  } from "../components";
 import { useEffect, useState } from "react";
+import { Layout } from "../layout/Layout";
 
 export default function Home(): JSX.Element {
   const [state, setState] = useState(0);
@@ -10,7 +11,7 @@ export default function Home(): JSX.Element {
   })
 
   return (
-    <>
+    <Layout>
       <Htag tag="h2">{state}</Htag>
       <Button appearance="primary" arrow="right" onClick={()=> setState(x => x+1)}>кнопка1</Button>
       <Button appearance="ghost" arrow="down" className="aslkjdkjsa">
@@ -20,6 +21,6 @@ export default function Home(): JSX.Element {
       </P>
       <Tag color="red" href="sadksd">Ghost</Tag>
       <Rating rating={rating} isEditable setRating={setRating}></Rating>
-    </>
+    </Layout>
   );
 }
